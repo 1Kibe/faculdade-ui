a99832654ab205a760b87ea562870c55b86e0a65
1Kibe <162029908+1Kibe@users.noreply.github.com>
2025-03-08 15:02:03 -0300

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {ButtonModule} from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
   declarations: [
@@ -10,6 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    CheckboxModule,
+    InputTextModule,
+    
+    
     AppRoutingModule
   ],
   providers: [],
