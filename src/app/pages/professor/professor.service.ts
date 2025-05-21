9be613ed92e35aa66a7c91e8338e0494fcb66442
1Kibe <ryanadrianamendoin@gmail.com>
2025-05-21 20:50:01 -0300

    mockProf

--- src/app/pages/professor/professor.service.ts
+++ src/app/pages/professor/professor.service.ts
@@ -1,21 +1,40 @@
 import { Injectable } from '@angular/core';
 import { Professor } from '../core/models/professor.model';
-import { DATA } from './data'; // Supondo que você esteja simulando com um mock
+import { firstValueFrom } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfessorService {
+ professorUrl: string = '';
 
-  constructor() {}
+  constructor(private http: HttpClient) {
+    this.professorUrl =
+    'https://67e4bd942ae442db76d56458.mockapi.io/professor';
+  }
 
   // Método para listar os professores, retorna uma promise
-  listar(): Promise<Professor[]> {
-    return new Promise((resolve) => {
-      // Simulando um delay, como se estivesse fazendo uma requisição HTTP
-      setTimeout(() => {
-        resolve(DATA); // Retorna os dados simulados
-      }, 1000); // Delay de 1 segundoa
-    });
-  }
+    listar(): Promise<any> {
+      return firstValueFrom(this.http.get(this.professorUrl)).then(
+        (response: any) => {
+          return response;
+        }
+      );
+    }
+    adicionar(obj: Professor): Promise<Professor> {
+        return firstValueFrom(this.http.post<Professor>(this.professorUrl, obj));
+      }
+    
+      buscarPorId(id: number) {
+        return firstValueFrom(this.http.get(`${this.professorUrl}/${id}`)).then(
+          (response) => response as any
+        );
+      }
+    
+      atualizar(obj: Professor): Promise<Professor> {
+        return firstValueFrom(
+          this.http.put<Professor>(`${this.professorUrl}/${obj.idprofessor}`, obj)
+        ).then((response) => response as Professor);
+      }
 }
